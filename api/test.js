b001399e53aacafe15ddea476c885ccacf957f2f
SirReGaDerEchte <77300282+SirReGaDerEchte@users.noreply.github.com>
2025-01-11 03:39:09 +0100

    Add school variable

--- api/test.js
+++ api/test.js
@@ -1,12 +1,13 @@
 const username = "" // your cevex/homeinfopoint username
 const password = "" // your cevex/homeinfopoint password
+const school = "" // your cevex/homeinfopoint password
 
 const response = await fetch("https://cevex.sirrega.de/data", {
     method: 'POST',
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
     },
-    body: `username=${username}&password=${password}`
+    body: `username=${username}&password=${password}&school=${school}`
 })
 
 console.log(await response.text())
